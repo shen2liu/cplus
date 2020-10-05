f50334ea47edc4b5571d009e3a00e5b1a1a78669
shen2liu <shen2liu@yahoo.com>
2020-10-05 11:24:02 -0400

    change heap.cpp to class

--- heap.cpp
+++ heap.cpp
@@ -1,345 +1,286 @@
-/***
- **** this is about the heap binary tree, not the heap memory ****
- ***/
-// Heap
+// k-Ary Heap Tree
 //   it is a special completed binary tree. there are three types of heaps:
-//      1) max heap, the node value is greater than the children, the root is the max.
-//      2) min heap, the node value is less than the children, the root is the min.
-//      3) min-max heap, the values are alternated on levels.
-//   To insert an element to a max-heap, 
-//      add the element to the end of the tree (or array), then check its parents
-//      swap if needed to make the tree be a max-heap again.
-//   To delete an element from a max-heap
-//      we can only delete the root element from a heap tree
-//      After remove the root node, bring the node at the tail to the root, then
-//      swap with the child who has the greater value unti the tree become a heap tree.
-//   To adjust a binary to be a heap
-//      start from the last element, for each node, check its children to see if it
-//      it is a heap, swap it and check its child if it is still a heap... 
-//   To sort the heap
-//      delete the root node and put it to the end, reduced the heap size by 1.
-//      the heap will be sorted after delete every element in the heap.
-//      the heap sort algorithm is first to insert elements to the heap, then delete
-//      every element.
-//   implemetations:
-//      array: the root is the first element, index is 0. 
-//             the left node of any node i must be at 2 * i + 1.
-//             the right node of any node i must be at 2 * i + 2.
-//      tree:  a complete binary tree, see TreeNode() in tree.cpp
-//   applications:
-//    - priority queues
-//    - k-way merge
-//    - shortest path first algorithms
+//   1) max heap, the node value is greater than the children, the root is the max.
+//   2) min heap, the node value is less than the children, the root is the min.
+//   3) min-max heap, the values are alternated on levels. // not implemented
+//
+// implementation:
+//   the heap class implements a k-ary min-heap or max-heap for any data types.
+//   the heap is stored in a vector which manages its memory automatically.
+//   the heap is represented by vector, each node (i) has k children.
+//   the index of a child is: i * k + m; 1 <= m <= k.
+//   it is a binary heap when k is equal to 2.
+//   the root is the first element, index is 0. 
+//   the root is minimum when the heap is min-heap.
+//   the root is maximum when the heap is max-heap.
+//
+// applications:
+//   - priority queues
+//   - k-way merge
+//   - shortest path first algorithms
+//   - etc.
 // 
 
 #include <iostream>
-// #define NDEBUG
-#include <assert.h>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
-/* Heap Adjust
- * algorithm: for the node (element) that has the index i,
- *            its left child is at 2*i, its right child is at 2*i + 1. 
- *            compare these three values, put the largest at i.
- *            continue the adjust if the node at i has been changed.
- */
+// HeapTree::type
+#define MIN_HEAP    0
+#define MAX_HEAP    1   
 
-/* adjust a binary tree downward to be a min-heap or max-heap
+template<class T>
+class HeapTree {
+    int size;           // number of elements in the heap
+    int type;           // heap type: 0 = min; 1 = max; else = undefined
+    int kAry;           // number of chilren
+protected:
+    vector<T> heap;
+    void build_heap(const vector<T>& v);
+    void adjust_down(int i);
+    void adjust_up(int i);
+public: 
+    HeapTree(const vector<T>& v) : size(v.size()), type(MAX_HEAP), kAry(2) { build_heap(v); };
+    HeapTree(const vector<T>& v, const int& t) : size(v.size()), type(t), kAry(2) { build_heap(v); }
+    HeapTree(const vector<T>& v, const int& t, const int& k) : size(v.size()), type(t), kAry(k) { build_heap(v); }
+
+    int  get_size() { return size; };
+    int  get_type() { return type; };
+    T pop();
+    void push(T x);
+    void sort();
+    void display_heap_array(string title);
+    void display_heap_tree();
+};
+
+template<class T>
+struct MinHeap : public HeapTree<T> {
+    MinHeap(const vector<T>& v) : HeapTree<T>(v, MIN_HEAP) {}   // binary min-heap
+    MinHeap(const vector<T>& v, const int& k) : HeapTree<T>(v, MIN_HEAP, k) {}  // k-ary min-heap
+};
+
+template<class T>
+struct MaxHeap : public HeapTree<T> {
+    MaxHeap(const vector<T>& v) : HeapTree<T>(v, MAX_HEAP) {}   // binary max-heap
+    MaxHeap(const vector<T>& v, const int& k) : HeapTree<T>(v, MAX_HEAP, k) {}  // k-ary max-heap
+};
+
+/* build_heap:
+ *   copy a vector into the heap class, and
+ *   turn it into a heap (tree).
  */
-void heap_adjust_down(long a[], int sz, int i, bool is_max)
+template<class T>
+void HeapTree<T>::build_heap(const vector<T>& v)
 {
-    int left = 2*i + 1;
-    int right = 2*i + 2;
-    int m = i;
-
-    if (sz <= 1 || i < 0) {
+    int sz = size;
+    if (sz <= 1) {
         return;
     }
-    else if (left >= sz) {
-        return;
-    }
-    else if (right >= sz) {
-        m = left;
-    }
-    else if (is_max) {
-        m = a[left] > a[right] ? left : right;
-    }
-    else {
-        m = a[left] < a[right] ? left : right;
-    }
+    heap = v;
 
-    bool adjust = is_max ? a[i] < a[m] : a[i] > a[m];
-    if (adjust) {
-        swap(a[i], a[m]);
-        heap_adjust_down(a, sz, m, is_max);
+    for (int i = sz/kAry - 1; i >= 0; --i) {
+        adjust_down(i);
     }
 }
-/* 
- * heapify (adjust to heap) bottom-up
- * best used for adding an element 
+/* adjust_down:
+ *   adjust an array to a heap using recursion,
+ *   starting from node i and going downward.
  */
-void heap_adjust_up(long a[], int sz, int i, bool is_max)
+template<class T>
+void HeapTree<T>::adjust_down(int i) 
 { 
-    if (sz <= 1 || i <= 0) {
+    if (i < 0 || size <= 1 || kAry < 1) {
         return;
     }
 
-    int parent = (i - 1) >> 1;  // (i - 1)/2
-    if (parent >= 0) { 
-        bool adjust = is_max ? a[i] > a[parent] : a[i] < a[parent];
-        if (adjust) { 
-            swap(a[i], a[parent]); 
-            heap_adjust_up(a, sz, parent, is_max); 
-        } 
+    int  mm_index = i;
+    T mm_child = heap[i];
+
+    for (int m = 1; m <= kAry; ++m) {  
+        int child_index = kAry * i + m;
+        if (child_index >= size) {
+            break;
+        }
+        if ( ((type == MIN_HEAP) && (heap[child_index] < mm_child)) ||  
+             ((type == MAX_HEAP) && (heap[child_index] > mm_child)) ) 
+        {
+            mm_index = child_index;
+            mm_child = heap[child_index];
+        }
+    }
+
+    if ( ((type == MIN_HEAP) && (heap[i] > heap[mm_index])) ||  
+         ((type == MAX_HEAP) && (heap[i] < heap[mm_index])) ) 
+    {
+        swap(heap[i], heap[mm_index]);
+        adjust_down(mm_index);
+    }
+} 
+/* adjust_up:
+ *   adjust an arry to be a heap recursively,
+ *   starting from node i and going upward.
+ */
+template<class T>
+void HeapTree<T>::adjust_up(int i) 
+{ 
+    if (i < 0 || size <= 1 || kAry < 1) {
+        return;
+    }
+    int parent = (i - 1) / kAry;
+    if ( ((type == MIN_HEAP) && (heap[i] < heap[parent])) ||  
+         ((type == MAX_HEAP) && (heap[i] > heap[parent])) ) 
+    {
+        swap(heap[i], heap[parent]);
+        adjust_up(parent);
     } 
 }
-/* make a binary tree to be a heap
- * must starting from bottom 
- * do not need to do anything for the bottom level
- */
-void max_heap_build(long a[], int sz)
-{
-    if ( sz <= 1) {
-        return;
-    }
-    for (int i = (sz >> 1) - 1; i >= 0; --i) {
-        heap_adjust_down(a, sz, i, true);
-    }
-}
-/*
- * build a min-heap from an array
- */
-void min_heap_build(long a[], int sz)
-{
-    if ( sz <= 1) {
-        return;
-    }
-    for (int i = (sz >> 1) - 1; i >= 0; --i) {
-        heap_adjust_down(a, sz, i, false);
-    }
-}
-/*
- * check if an array is a min-heap
- */
-bool is_min_heap_array(long hp[], int sz)
-{
-    if (!hp || !sz) {
-        return false;
-    }
-    if (sz == 1) {
-        return true;
-    }
-
-    int left, right;
-    for (int i = 0; i < (sz >> 1); ++i) {
-        left = 2*i + 1;
-        if (left >= sz ) {
-            break;
-        }
-        if (hp[i] > hp[left]) {
-            return false;
-        }
-        right = 2*i + 2;
-        if (right >= sz) {
-            break;
-        }
-        if (hp[i] > hp[right]) {
-            return false;
-        }
-    }
-    return true;    
-}
-/*
- * check if an array is a max-heap
- */
-bool is_max_heap_array(long hp[], int sz)
-{
-    if (!hp || !sz) {
-        return false;
-    }
-    if (sz == 1) {
-        return true;
-    }
-
-    int left, right;
-    for (int i = 0; i < (sz >> 1); ++i) {
-        left = 2*i + 1;
-        if (left >= sz ) {
-            break;
-        }
-        if (hp[i] < hp[left]) {
-            return false;
-        }
-        right = 2*i + 2;
-        if (right >= sz) {
-            break;
-        }
-        if (hp[i] < hp[right]) {
-            return false;
-        }
-    }
-    return true;    
-}
-/*
- * check if an array is a heap array
- */
-bool is_heap_array(long hp[], int sz)
-{
-    if (hp[0] < hp[sz - 1] )  // min_heap
-    {
-        return is_max_heap_array(hp, sz);
-    }
-    else  // min-heap
-    {
-        return is_min_heap_array(hp, sz);
-    }
-}
-/* 
- * remove and return the root (max or min) element.
+/* pop:
+ *   remove and return the root (max or min) element.
  * algorithm: 
- *    save the root, 
- *    move the last element to the root,
- *    adjust the heap top-down.
- * parameters: 
- *    (long)hp[] - heap array
- *    (int)sz    - array size, must > 0
- * Note:
- *    this implementation does not change the array size,
- *    clear the (last) element that is not in use.
- *    the caller needs to rmember the heap size.
- */            
-long heap_pop(long hp[], int sz)
+ *   save the root;
+ *   move the last element to the root;
+ *   adjust the heap top-down;
+ *   return the root;
+ */
+template<class T>
+T HeapTree<T>::pop()
 {
-    // cannot return anything, just abort
-    assert(sz > 0);
-
-    bool is_max = (sz > 1) && (hp[0] > hp[sz - 1]);
-    long root = hp[0];
-    --sz;
-    hp[0] = hp[sz], hp[sz] = 0;
-    heap_adjust_down(hp, sz, 0, is_max);
+    T root = heap[0];
+    --size;
+    heap[0] = heap[size];
+    heap.pop_back();
+    adjust_down(0);
     return root;
 }
-/* 
- * add the new element at the end of the tree (heap array).
+/* push:
+ *   add the new element at the end of the tree (heap array).
  * algorithm:
  *   add the new element at the end of the heap.
  *   adjust the heap from bottom up.
- * note:
- *   make sure the array has an extra space for the new element.
- *   caller needs to remember the heap size.
  */
-int heap_push(long hp[], int sz, int data)
+template<class T>
+void HeapTree<T>::push(T x)
 {
-    bool is_max = (sz > 1) && (hp[0] > hp[sz - 1]);
-    hp[sz - 1] = data;
-    heap_adjust_up(hp, sz, sz - 1, is_max);     // 
-    return hp[0];
+    heap.push_back(x);
+    ++size;
+    adjust_up(size - 1);
 }
-/* Heap Sort
- * algorithm: to easily calculate the heap tree index, we need index stat from 1.
- *            so make a new array its size 1 bigger than the orignal array.
- *            convert (adjust) the array to a heap tree, start at the middle.
- *            copy the root node to the far right, reduce the array size by 1.
- *            adjust the heap tree make sure the element at 1 is the root (max).
- *            contine the last two steps until array size is 0.
- * applications: priority queues
+/* sort:
+ *   sorting the vector in asending order using max-heap or 
+ *   desending order using min-heap.
+ * algorithm:
+ *   convert the vector into a heap (vector),
+ *   pop the first element and move it to the end,
+ *   reduce the size of the heap,
+ *   repeat the previous steps until only one element left,
+ *   restore the size of the heap.
  */
-void heap_sort(long hp[], int sz)
+template<class T>
+void HeapTree<T>::sort()
 {
-    if (sz <= 1)  return;
-
-    bool is_max = hp[0] > hp[sz - 1];
+    int sz = size;
     for (int i = sz - 1; i >= 1; i--) {
-        swap(hp[i], hp[0]);
-        heap_adjust_down(hp, i, 0, is_max);
+        swap(heap[i], heap[0]);
+        --size;
+        adjust_down(0);
     }
+    size = sz;
 }
-
-/* tesing driver code
+/* display_heap_array:
+ *    display the heap data in array format.
+ * note:
+ *    only display the data in the beginning and at the end,
+ *    when the size of heap greater than 32.
  */
-#include <cmath>
-
-/* display heap data in array format
- */
-void heap_array_display(string title, long hp[], int sz)
+template<class T>
+void HeapTree<T>::display_heap_array(string title)
 {
-    cout << "\e[1m" << title << "\e[0m" << ": " << "A[" << sz << "] = "; 
-    for (int i = 0; i < sz; ++i) { cout << hp[i] << ", "; } cout << endl;
-}
-/* display heap data by tree levels
- */
-void heap_tree_display(long hp[], int sz)
-{
-    if (sz <= 0)  return;
+    const int words_line = min(16, size);
+    const int word_width = log10(size) + 2;
 
-    // const int w = log10(sz * 10) + 1;
-    
-    cout << " root:  " << "[--0] = " << hp[0] << endl;
-    for (int i = 1, n = 1; n < sz; ++i) {
-        cout.width(5); cout << i << ":  ";
-        int m = n + pow(2, i);
-        for (int j = n; (j < m) && (n < sz); ++j, ++n) {
-            cout << "[" << (j - 1)/2 << "-" << j << "] = " << hp[j] << "  ";
+    cout << "\e[1m" << title << "\e[0m" << ": " << endl;
+    cout << "H[" << size << "] = "; 
+    for(unsigned i = 0; i < words_line; ++i) {
+        cout.width(word_width); cout << heap[i] << ", "; 
+    } 
+    cout << endl;
+    if (size > words_line) {
+        for(int i = 0; i < word_width + 5; ++i) { 
+            cout << " "; 
+        }
+        for(int i = size - min(size - words_line, words_line); i < size; ++i) {
+            cout.width(word_width); cout << heap[i] << ", "; 
         }
         cout << endl;
     }
 }
-/* testing main: could take one argument as the size of the heap array
+/* display_heap_tree:
+ *   display the heap data by tree levels.
+ *   [p-c] shows the parent index and child index.
+ */
+template<class T>
+void HeapTree<T>::display_heap_tree()
+{
+    if (size <= 0)  return;
+
+    cout << " root:  " << "[--0] = " << heap[0] << endl;
+    for (int i = 1, n = 1; n < size; ++i) {
+        cout.width(5); cout << i << ":  ";
+        int m = n + pow(kAry, i);
+        for (int j = n; (j < m) && (n < size); ++j, ++n) {
+            cout << "[" << (j - 1)/kAry << "-" << j << "] = " << heap[j] << "  ";
+        }
+        cout << endl;
+    }
+}
+/* main:
+ *   testing driver main.
+ *   could take one argument as the size of the heap.
  */
 int main(int argc, char *argv[])
 {   
     int n = 32;
     if (argc > 1) {
-        if ((n = atoi(argv[1])) < 0)
+        if ((n = atoi(argv[1])) < 0){
             return -1;
+        }
     }
     else {
         n = rand() % n;
     }
 
-    int heap_size = n;
-    long A[n];  // testing heap array
-    long B[n]; 
+    long A[n];
 
-    // create an array with n of random data
-    for(int i = 0; i < n; ++i) { B[i] = rand() % (n * 10); }
-    heap_array_display("Heap Array", B, n);
+    for(int i = 0; i < n; ++i) { A[i] = rand() % (n * 10); }
+    cout << "\e[1m" << "Original Array" << "\e[0m" << ": " << endl;
+    cout << "A[" << n << "] = "; 
+    for(int i = 0; i < min(16, n); ++i) { cout.width(log10(n) + 2); cout << A[i] << ", "; } cout << endl;
+    cout << "        ";
+    for(int i = n - min(n - 16, 16); i < n; ++i) { cout.width(log10(n) + 2); cout << A[i] << ", "; } cout << endl;
 
-    copy(B, B + n, A);
-    max_heap_build(A, n);
-    heap_array_display("Max-Heap Build", A, n);
-    heap_tree_display(A, n);
+    vector<long> v(A, A + n);
+    HeapTree<long> hv(v, 1, 3);
+    hv.display_heap_array("Heap Vector");
+    hv.display_heap_tree();
 
-    long d = heap_pop(A, n);
-    heap_size -= 1;
-    heap_array_display("Heap Pop", A, n);
-    heap_tree_display(A, n);
-    
-    heap_push(A, n, d);
-    heap_size += 1;
-    heap_array_display("Heap Push", A, n);
-    heap_tree_display(A, n);
+    long d = hv.pop();
+    hv.display_heap_array("Heap Pop");
+    // hv.display_heap_tree();
+    hv.push(d);
+    hv.display_heap_array("Heap Push");
+    // hv.di splay_heap_tree();
 
-    heap_sort(A, n);
-    heap_array_display("Heap Sort", A, n);
-    heap_sort(A, n);
-    heap_array_display("Heap Sort", A, n);
+    MinHeap<long> min_heap(v);
+    min_heap.display_heap_array("Min-Heap");
+    min_heap.sort();
+    min_heap.display_heap_array("Min-Heap Sort");
 
-    copy(B, B + n, A);
-    min_heap_build(A, n);
-    heap_array_display("Min-Heap Build", A, n);
-    heap_tree_display(A, n);
-
-    A[n - 1] = 64;
-    heap_array_display("Is Heap Array", A, n);
-    bool b = is_heap_array(A, n);
-    if (b) { cout << "Is a Heap"; }  else { cout << "Not a Heap"; }  cout << endl;
-
-    // heap_adjust_down(A, n, (n >> 1) - 1, false);
-    heap_adjust_up(A, n, n - 1, false);
-    heap_array_display("Heap Adjust", A, n);
+    MaxHeap<long> max_heap(v);
+    max_heap.display_heap_array("Max-Heap");
+    max_heap.sort();
+    max_heap.display_heap_array("Max-Heap Sort");
 
     return 0;
 }
